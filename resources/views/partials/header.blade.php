d641c7c5e403591eb694f80b51acfd54063573cb
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-10 15:56:05 +0200

    Import general, mixin, variables and header style and import header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="container">
+        <a href="/" class="brand">
+            <img src="/img/dc-logo.png" alt="logo">
+        </a>
+
+        <!-- Navigazione -->
+        <nav>
+            <ul>
+                <li><a href="#" class="">CHARACTERS</a></li>
+                <li><a href="#" class="active">COMICS</a></li>
+                <li><a href="#" class="">MOVIES</a></li>
+                <li><a href="#" class="">TV</a></li>
+                <li><a href="#" class="">GAMES</a></li>
+                <li><a href="#" class="">COLLECTIBLES</a></li>
+                <li><a href="#" class="">VIDEOS</a></li>
+                <li><a href="#" class="">FANS</a></li>
+                <li><a href="#" class="">NEWS</a></li>
+                <li><a href="#" class="">SHOP</a></li>
+
+            </ul>
+        </nav>
+    </div>
+
+</header>
